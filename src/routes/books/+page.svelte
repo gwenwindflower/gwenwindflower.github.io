d7d043ea957ba84dca55c864c7d00575098221e1
gwen windflower <gwenwindflower@gmail.com>
2023-01-05 15:49:48 -0600

    Get genre filter buttons working

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -3,20 +3,26 @@
 
 	export let data;
 	// TODO: figure out a way to build this list from the markdown files
-	const genres = ['modern', 'sci-fi', 'short stories', 'horror'];
-	let genresSelected = genres;
+	let selected = {
+		'sci-fi': true,
+		'short stories': true,
+		horror: true,
+		modern: true
+	};
+	const genres = Object.keys(selected);
+
 	function pickGenre(genre) {
-		genresSelected = genre;
+		selected[genre] = !selected[genre];
 	}
 	$: filteredBooks = data.books.filter((book) =>
-		book.meta.genres.some((bookGenre) => genresSelected.includes(bookGenre))
+		book.meta.genres.some((bookGenre) => selected[bookGenre])
 	);
 </script>
 
 <h1>These are all the books I've read</h1>
 <p>Feel free to filter by genre.</p>
 {#each genres as genre}
-	<button on:click={() => pickGenre(genre)}>{genre}</button>
+	<button class:selected={selected[genre]} on:click={() => pickGenre(genre)}>{genre}</button>
 {/each}
 <ul>
 	<!-- TODO: add genre filters to this list -->
@@ -24,3 +30,10 @@
 		<li><a href={book.path}>{book.meta.title}</a></li>
 	{/each}
 </ul>
+
+<style>
+	.selected {
+		background-color: blue;
+		color: white;
+	}
+</style>
