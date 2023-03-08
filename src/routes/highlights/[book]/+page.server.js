4f0e7ddb7f5521ce17a3ce94d12c0a8ad48a4291
gwen windflower <gwenwindflower@gmail.com>
2023-03-08 12:45:12 -0600

    Trying something goofier

--- src/routes/highlights/[book]/+page.server.js
+++ src/routes/highlights/[book]/+page.server.js
@@ -1,15 +1,17 @@
 import { READWISE_TOKEN } from '$env/static/private';
 
-let token;
-if (!READWISE_TOKEN) {
-	token = process.env.READWISE_TOKEN;
+let myKey;
+if (process.env.READWISE_TOKEN) {
+	myKey = process.env.READWISE_TOKEN;
+} else {
+	myKey = READWISE_TOKEN;
 }
 
 export async function load({ params, fetch }) {
 	const response = await fetch(`https://readwise.io/api/v2/export?ids=${params.book}`, {
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Token ${READWISE_TOKEN | token}`
+			Authorization: `Token ${myKey}`
 		}
 	});
 	const parsedResponse = await response.json();
