be8e5c562437be884da59d3551507dbaf6da7eea
gwen windflower <gwenwindflower@gmail.com>
2023-03-08 12:14:56 -0600

    Trying something goofy

--- src/routes/highlights/+page.server.js
+++ src/routes/highlights/+page.server.js
@@ -1,10 +1,15 @@
 import { READWISE_TOKEN } from '$env/static/private';
 
+let token;
+if (!READWISE_TOKEN) {
+	token = process.env.READWISE_TOKEN;
+}
+
 export async function load({ fetch }) {
 	const response = await fetch(`https://readwise.io/api/v2/export/`, {
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Token ${READWISE_TOKEN}`
+			Authorization: `Token ${READWISE_TOKEN | token}`
 		}
 	});
 	const parsedResponse = await response.json();
--- src/routes/highlights/[book]/+page.server.js
+++ src/routes/highlights/[book]/+page.server.js
@@ -1,10 +1,15 @@
 import { READWISE_TOKEN } from '$env/static/private';
 
+let token;
+if (!READWISE_TOKEN) {
+	token = process.env.READWISE_TOKEN;
+}
+
 export async function load({ params, fetch }) {
 	const response = await fetch(`https://readwise.io/api/v2/export?ids=${params.book}`, {
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Token ${READWISE_TOKEN}`
+			Authorization: `Token ${READWISE_TOKEN | token}`
 		}
 	});
 	const parsedResponse = await response.json();
