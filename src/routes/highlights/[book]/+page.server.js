2fcb0904365a821cf352e581d060568581764e3f
gwen windflower <gwenwindflower@gmail.com>
2023-03-08 12:55:07 -0600

    Trying something goofiest

--- src/routes/highlights/[book]/+page.server.js
+++ src/routes/highlights/[book]/+page.server.js
@@ -3,6 +3,7 @@ import { READWISE_TOKEN } from '$env/static/private';
 let myKey;
 if (process.env.READWISE_TOKEN) {
 	myKey = process.env.READWISE_TOKEN;
+	console.log('poooooooop');
 } else {
 	myKey = READWISE_TOKEN;
 }
